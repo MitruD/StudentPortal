54773cff625506d747ab71a78621fde7b2907e26
Kronos <dimamitru@gmail.com>
2024-09-28 11:57:39 +0300

    Add project files.

---
+++ MVC_EF_SQL_CRUD_StudentPortalWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_EF_SQL_CRUD_StudentPortalWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
